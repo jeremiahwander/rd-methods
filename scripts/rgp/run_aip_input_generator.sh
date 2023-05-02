9965cb9c313bd9b7fc0863a2a3f2ddacbf621ee5
Miah Wander <miah@microsoft.com>
2023-05-02 23:24:02 +0000

    tag generation scripts

--- scripts/rgp/run_aip_input_generator.sh
+++ scripts/rgp/run_aip_input_generator.sh
@@ -4,4 +4,4 @@ python3 scripts/rgp/rgp_aip_input_generator.py \
     --seqr_indiv "$HOME/data/rgp/metadata/rare_genomes project_genomes_gru_individuals.tsv" \
     --seqr_indiv "$HOME/data/rgp/metadata/rare_genomes project_genomes_hmb_individuals.tsv" \
     --group_assignments "$HOME/data/rgp/group_assignments.tsv" \
-    --output_dir "$HOME/data/rgp/air_inputs"
+    --output_dir "$HOME/data/rgp/aip_inputs"
